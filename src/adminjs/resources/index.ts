e4b0bdd6f1934bae481dcdccc5ff6b2f2cb014eb
Gabriela Florêncio <perflogabi@gmail.com>
2023-12-06 21:08:17 -0500

    upload de arquivos no AdminJs

--- src/adminjs/resources/index.ts
+++ src/adminjs/resources/index.ts
@@ -1,10 +1,12 @@
 import { ResourceWithOptions } from "adminjs";
 import { Category } from "../../models";
 import { categoryResourceOptions } from "./category";
+import { episodeResourceFeatures } from "./episode";
 
 export const adminJsResources: ResourceWithOptions[] = [
   {
     resource: Category,
-    options: categoryResourceOptions
+    options: categoryResourceOptions,
+    features: episodeResourceFeatures
   }
 ]
