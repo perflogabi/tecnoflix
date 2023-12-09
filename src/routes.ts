00a908c7a423ee5c884eaa10f7454f791f7c83bf
Gabriela Florêncio <perflogabi@gmail.com>
2023-12-09 17:54:52 -0500

    protegendo os videos

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,9 @@
 import express from 'express'
 import { categoriesController } from './controllers/categoriesController'
 import { coursesController } from './controllers/coursesController'
-import { authController } from './controllers/authController.'
+import { authController } from './controllers/authController'
 import { episodesController } from './controllers/episodesController'
-import { ensureAuth } from './middlewares/auth'
+import { ensureAuth, ensureAuthViaQuery } from './middlewares/auth'
 
 const router = express.Router()
 
@@ -18,7 +18,7 @@ router.get('/courses/newest', coursesController.newest)
 router.get('/courses/search', ensureAuth, coursesController.search)
 router.get('/courses/:id', ensureAuth, coursesController.show)
 
-// router.get{'/episodes/stream', episodesController.stream}
+router.get( '/episodes/stream', ensureAuthViaQuery, episodesController.stream)
 
 
 
