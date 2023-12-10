8b11a87b78b60ba668bb7e16a49d31272da6ef7f
Gabriela Florêncio <perflogabi@gmail.com>
2023-12-09 19:01:02 -0500

    adicionando favoritos

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,19 +1,28 @@
 import { Category } from './Category'
 import { Course } from './Course'
 import { Episode } from './Episode'
+import { Favorite } from './Favorite'
 import { User } from './User'
 
-Category.hasMany(Course, { as: 'courses' })
-Course.hasMany(Episode, { as: 'episodes' })
+Category.hasMany(Course)
 
 Course.belongsTo(Category)
 Course.hasMany(Episode)
+Course.belongsToMany(User, { through: Favorite })
+Course.hasMany(Favorite, { as: 'favoritesUsers', foreignKey: 'course_id' })
 
 Episode.belongsTo(Course)
 
+Favorite.belongsTo(Course)
+Favorite.belongsTo(User)
+
+User.belongsToMany(Course, { through: Favorite })
+User.hasMany(Favorite, { as: 'favoritesCourses', foreignKey: 'user_id' })
+
 export {
-  Course,
   Category,
+  Course,
   Episode,
+	Favorite,
   User
 }
--- src/routes.ts
+++ src/routes.ts
@@ -4,6 +4,7 @@ import { coursesController } from './controllers/coursesController'
 import { authController } from './controllers/authController'
 import { episodesController } from './controllers/episodesController'
 import { ensureAuth, ensureAuthViaQuery } from './middlewares/auth'
+import { favoritesController } from './controllers/favoritesController'
 
 const router = express.Router()
 
@@ -19,7 +20,7 @@ router.get('/courses/search', ensureAuth, coursesController.search)
 router.get('/courses/:id', ensureAuth, coursesController.show)
 
 router.get( '/episodes/stream', ensureAuthViaQuery, episodesController.stream)
-
+router.post('/favorites', ensureAuth, favoritesController.save)
 
 
 export { router }
