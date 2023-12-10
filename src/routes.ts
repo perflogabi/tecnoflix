1365099a0643040519d308c19be44c217c44a703
Gabriela Florêncio <perflogabi@gmail.com>
2023-12-09 19:45:41 -0500

    obtendo e excluindo os cursos favoritos

--- src/routes.ts
+++ src/routes.ts
@@ -22,5 +22,9 @@ router.get('/courses/:id', ensureAuth, coursesController.show)
 router.get( '/episodes/stream', ensureAuthViaQuery, episodesController.stream)
 router.post('/favorites', ensureAuth, favoritesController.save)
 
+router.get('/favorites', ensureAuth, favoritesController.index)
+router.post('/favorites', ensureAuth, favoritesController.save)
+
+
 
 export { router }
